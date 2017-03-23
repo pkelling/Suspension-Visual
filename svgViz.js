487a9537cd8bed86844cc9868f721dde4095d09f
PK <pkelling94@gmail.com>
2017-03-23 00:36:38 -0500

    Only convert to negative z values for svg graph at the end

--- svgViz.js
+++ svgViz.js
@@ -29,7 +29,7 @@ var drawSuspension = function(L_upper = 300,
                                 A_lower0 = 0, 
                                 kpi = 0, 
                                 scrubR = 100, 
-                                Zb = -100, 
+                                Zb = 100, 
                                 knuckle = 200){
 
     
@@ -38,7 +38,7 @@ var drawSuspension = function(L_upper = 300,
 
     //determined from Variables
     //Be careful with those negative z values!
-    var yBE = Snap.tan(kpi)*(-Zb)+scrubR;
+    var yBE = Snap.tan(kpi)*(Zb)+scrubR;
 
     var By0 = Snap.cos(A_lower0)*L_lower;
     var Bz0 = Zb;
@@ -47,16 +47,16 @@ var drawSuspension = function(L_upper = 300,
     Ez0 = 0;
 
     Ay0 = 0;
-    Az0 = Snap.sin(A_lower0)*L_lower+Zb;
+    Az0 = Zb-Snap.sin(A_lower0)*L_lower;
 
     var Dy0 = By0-knuckle*Snap.sin(kpi);
-    var Dz0 = Zb-knuckle*Snap.cos(kpi);
+    var Dz0 = Zb+knuckle*Snap.cos(kpi);
 
     Cy0 = Dy0-Snap.cos(A_upper0)*L_upper;
-    Cz0 = Dz0+Snap.sin(A_upper0)*L_upper;
+    Cz0 = Dz0-Snap.sin(A_upper0)*L_upper;
 
     L_eb = Math.sqrt(Math.pow(yBE,2)+Math.pow(Zb,2));
-    A_eb0 = -Snap.atan(Zb/yBE);
+    A_eb0 = Snap.atan(Zb/yBE);
 
     A_bd0 = 90-kpi;
 
@@ -65,7 +65,7 @@ var drawSuspension = function(L_upper = 300,
 
     //////////////////////////////////////
     
-    var connect = s.line(connectLY0,connectLZ0, Ey0 - 50 ,connectLZ0).attr({
+    var connect = s.line(connectLY0,-connectLZ0, Ey0 - 50 ,-connectLZ0).attr({
         fill: "#fc0",
         stroke: "#000",
         strokeWidth: 2
@@ -95,33 +95,33 @@ var drawSuspension = function(L_upper = 300,
     */
     
     //lower pivot
-    s.circle(Ay0,Az0,5);
+    s.circle(Ay0,-Az0,5);
     
     //upper pivot
-    s.circle(Cy0,Cz0,5);
+    s.circle(Cy0,-Cz0,5);
 
     //lower ball joint
-    s.circle(By0,Bz0,5);
+    s.circle(By0,-Bz0,5);
     
     //upper ball joint
-    s.circle(Dy0,Dz0,5);
+    s.circle(Dy0,-Dz0,5);
 
     //lower Arm
-    s.line(Ay0,Az0, By0,Bz0).attr({
+    s.line(Ay0,-Az0, By0,-Bz0).attr({
             fill: "#fc0",
             stroke: "#000",
             strokeWidth: 2
         });
 
     //upper Arm 
-    s.line(Cy0,Cz0, Dy0,Dz0).attr({
+    s.line(Cy0,-Cz0, Dy0,-Dz0).attr({
             fill: "#fc0",
             stroke: "#000",
             strokeWidth: 2
         });
 
     //knuckle
-    s.line(By0,Bz0, Dy0,Dz0).attr({
+    s.line(By0,-Bz0, Dy0,-Dz0).attr({
             fill: "#fc0",
             stroke: "black",
             strokeWidth: 5
@@ -180,9 +180,9 @@ var showDisplacement = function(){
 
     var A_lower = A_lower0 + displacement;
     var By = Snap.cos(A_lower)*L_lower;
-    var Bz = Az0 - Snap.sin(A_lower)*L_lower;
+    var Bz = Az0 + Snap.sin(A_lower)*L_lower;
 
-    var h = Bz-Cz0;
+    var h = Cz0 - Bz;
     var w = By - Cy0;
     var L_bc = Math.sqrt(h*h + w*w);
 
@@ -195,31 +195,31 @@ var showDisplacement = function(){
     var A_eb = A_eb0 + camber;
 
     var Ey = By + Snap.cos(A_eb)*L_eb;
-    var Ez = -Bz - Snap.sin(A_eb)*L_eb;
+    var Ez = Bz - Snap.sin(A_eb)*L_eb;
 
-    var bump = -Ez;
+    var bump = Ez;
     var scrub = Ey-Ey0;
 
-    var Dy = By - Snap.cos(A_bd)*knuckle;
-    var Dz = Bz - Snap.sin(A_bd)*knuckle;
+    var Dy = By + Snap.cos(A_bd)*knuckle;
+    var Dz = Bz + Snap.sin(A_bd)*knuckle;
 
     var connectLY = (By+Dy)/2;
     var connectLZ = (Bz+Dz)/2;
 
 //////////////////////////////////////
     
-    var lowerArm = s.line(Ay0,Az0, By,Bz).attr({
+    var lowerArm = s.line(Ay0,-Az0, By,-Bz).attr({
             stroke: "white",
             strokeWidth: 2
         });
-    var upperArm = s.line(Cy0,Cz0, Dy,Dz).attr({
+    var upperArm = s.line(Cy0,-Cz0, Dy,-Dz).attr({
             stroke: "white",
             strokeWidth: 2
         });
     
-    var lbj1 = s.circle(By,Bz,5).attr({fill:"orange"});
-    var ubj1 = s.circle(Dy,Dz,5).attr({fill:"red"});
-    var knuckle1 = s.line(By,Bz,Dy,Dz).attr({
+    var lbj1 = s.circle(By,-Bz,5).attr({fill:"orange"});
+    var ubj1 = s.circle(Dy,-Dz,5).attr({fill:"red"});
+    var knuckle1 = s.line(By,-Bz,Dy,-Dz).attr({
             fill: "#fc0",
             stroke: "#000",
             strokeWidth: 2
@@ -227,7 +227,7 @@ var showDisplacement = function(){
 
     knuckleAssembly = s.g(lowerArm,upperArm,lbj1,ubj1,knuckle1);
 
-    var connect1 = s.line(connectLY0,connectLZ0, Ey0 - 50 ,connectLZ0).attr({
+    var connect1 = s.line(connectLY0,-connectLZ0, Ey0 - 50 ,-connectLZ0).attr({
             stroke: "white",
             strokeWidth: 2
         });
@@ -241,11 +241,11 @@ var showDisplacement = function(){
     wheelAssembly = s.g(wheel1,contact1,connect1);
 
     var myMatrix = new Snap.Matrix();
-    myMatrix.rotate(camber,connectLY,connectLZ);
-    myMatrix.translate(connectLY-connectLY0,connectLZ-connectLZ0)
+    myMatrix.rotate(camber,connectLY,-connectLZ);
+    myMatrix.translate(connectLY-connectLY0,-connectLZ+connectLZ0)
     wheelAssembly.transform(myMatrix.toTransformString());
     
-    document.getElementById("bump").innerHTML = "Bump: " + Math.round(bump*-100)/100;
+    document.getElementById("bump").innerHTML = "Bump: " + Math.round(bump*100)/100;
     document.getElementById("camber").innerHTML = "Camber: " + Math.round(camber*100)/100;
 }
 
